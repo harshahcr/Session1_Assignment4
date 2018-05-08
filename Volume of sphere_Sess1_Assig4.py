5dd05d3d9d0e68e5a6c2db7e72af2b3dbdb80366
harshahcr <39063531+harshahcr@users.noreply.github.com>
2018-05-09 02:02:51 +0530

    Add files via upload

---
+++ Volume of sphere_Sess1_Assig4.py
@@ -0,0 +1,12 @@
+#Volume of sphere with dia = 12cms; v=4/3pir^3
+#Define values for variables 
+d=12
+print('The diameter of the sphere is d =', d ,'cm')
+r=int(d/2)
+print('The radius of the sphere derived r =',r ,'cm')
+#import the math lib for pi values
+import math
+pi1=math.pi
+v=4/3*pi1*r**3
+print('The volume of the sphere with diameter 12cms is ',v)
+
